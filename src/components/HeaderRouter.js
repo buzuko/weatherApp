8b71f8a688e93e12c676559b2bf28c4b32012b01
buzuko <dekel.eldar@gmail.com>
2023-08-29 14:08:11 +0300

    mission11 first commit

--- src/components/HeaderRouter.js
+++ src/components/HeaderRouter.js
@@ -1,12 +1,12 @@
-import React from "react";
+import { React, useEffect, useContext, useState } from "react";
 import '../App.css';
 import { Link } from "react-router-dom";
 function HeaderRouter() {
-
     return (
         <div className="HeaderRouter">
-            <Link to={"/"} className="links">ראשי</Link>
-            <Link to={"/"} className="links">משני</Link>
+            <Link to={"/Login"} className="LoginLink" >ראשי</Link>
+            <Link to={"/WeatherPage"} className="WeatherLink" >ראשי</Link>
+            <button className="HeaderRouterExit">exit</button>
         </div>
     )
 }
